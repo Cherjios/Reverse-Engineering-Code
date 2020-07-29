27535b10731d534cf5ae16ee65b471aff5da3372
cherjios <slopez0286@gmail.com>
2020-07-29 15:26:12 -0700

    Adding comments to passport.serializeUser and deserializeUser

--- Develop/config/passport.js
+++ Develop/config/passport.js
@@ -4,11 +4,11 @@
 // Passport is authentication middleware for Node.js. Extremely flexible and modular, Passport can be unobtrusively dropped in to any Express-based web application. 
 //A comprehensive set of strategies support authentication using a username and password, Facebook, Twitter, and more.
 
-// calling passport that requires passport
+// calling passport that requires the modules on passport
 var passport = require("passport");
 
-// calling LocalStrategy that require passport-local
-var LocalStrategy = require("passport-local").Strategy;
+// calling LocalStrategy that requires the modules on passport-local
+var LocalStrategy = require("passport-local").Strategy;  //This module lets you authenticate using a username and password in your Node.js applications.
 
 // db require modules in index.js and user.js
 var db = require("../models");
@@ -17,23 +17,25 @@ var db = require("../models");
 passport.use(new LocalStrategy(
   // Our user will sign in using an email, rather than a "username"
   {
-    usernameField: "email"
+    usernameField: "email" //giving usernameFiled an email
   },
   function(email, password, done) {
     // When a user tries to sign in this code runs
-    db.User.findOne({
+    //The function takes three parameters email, password and callback function done
+    db.User.findOne({ //In this line we are calling the function fineOne to compare 
+      //if the email typed by the user  exist in the table user
       where: {
         email: email
       }
     }).then(function(dbUser) {
       // If there's no user with the given email
-      if (!dbUser) {
-        return done(null, false, {
-          message: "Incorrect email."
+      if (!dbUser) { // if the email as input does not exist in the table user,the function calls the callback function
+        return done(null, false, { //callback function sends messages "Incorrect email" 
+          message: "Incorrect email." 
         });
       }
       // If there is a user with the given email, but the password the user gives us is incorrect
-      else if (!dbUser.validPassword(password)) {
+      else if (!dbUser.validPassword(password)) { //callback function sends messages "Incorrect incorrect password" if password does not match on from user table
         return done(null, false, {
           message: "Incorrect password."
         });
@@ -47,6 +49,8 @@ passport.use(new LocalStrategy(
 // In order to help keep authentication state across HTTP requests,
 // Sequelize needs to serialize and deserialize the user
 // Just consider this part boilerplate needed to make it all work
+
+// in order to support login sessions, Passport will serialize and deserialize user instances to and from the session.
 passport.serializeUser(function(user, cb) {
   cb(null, user);
 });
