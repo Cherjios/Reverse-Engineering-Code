5f940c4742e2d013ec61bf0d05ce9ec242465a75
cherjios <slopez0286@gmail.com>
2020-07-29 20:43:59 -0700

    Adding comments to index.js

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,10 +1,16 @@
 'use strict';
-
+// The fs module provides an API for interacting with the file system in a manner closely modeled around standard POSIX functions.
 var fs        = require('fs');
+//The Path module provides a way of working with directories and file paths.
 var path      = require('path');
+//Sequelize is a promise-based ORM for Node.js. It works with PostgreSQL, MySQL, SQLite and MSSQL dialects and features solid transaction support, relations, read replication and more.
 var Sequelize = require('sequelize');
+// The path.basename() method returns the filename part of a file path.
 var basename  = path.basename(module.filename);
+//the Express web server framework popularized using an environment variable called NODE_ENV as a flag to indicate whether 
+//the server should be running in “development” mode vs “production” mode. At runtime, the script looks up that value by checking process.env.NODE_ENV.
 var env       = process.env.NODE_ENV || 'development';
+// Config calls the dependencies from the configuration file
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
